c8bf63b68b94c38c55f3bd6b455cdcb2ff2b78ad
Fatahilla Afnan <52237684+Fanan2698@users.noreply.github.com>
2021-01-16 09:31:57 +0700

    Add files via upload

---
+++ migrations/versions/21eb3a2e7489_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 21eb3a2e7489
+Revises: 
+Create Date: 2021-01-07 20:55:41.761854
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '21eb3a2e7489'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
